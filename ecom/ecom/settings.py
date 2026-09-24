cf7d39d5b81cc0012d72fecc21c6d479d2443b3d
utkarshgupta-ai <utkarshgupta1312004@gmail.com>
2026-09-24 18:57:38 +0530

    Update settings.py

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -161,7 +161,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     try:
-       pass
+        import dj_database_url
+        DATABASES = {
+            'default': dj_database_url.config(
+                default=DATABASE_URL,
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True if ('postgres' in DATABASE_URL or 'psql' in DATABASE_URL) else False,
+            )
+        }
     except Exception:
         DATABASES = {
             'default': {
